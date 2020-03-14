dad1fbb91c7a0a58d4605ddc057ca354404c2dc7
Mahmoud Bdeir <mbdeir@aub.edu.lb>
2020-03-14 18:00:48 +0200

    The need for a Logger.DLL

---
+++ Driver/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+
+namespace Logger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger.Log("Program Started");
+            Thread.Sleep(3000); //Simulating work by having the program sleep for 3 seconds
+            Logger.Log("Program Ended");
+        }
+    }
+}
---
+++ Logger253/Logger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Logger
+{
+    public class Logger
+    {
+        public static void Log(string msg)
+        {
+            Console.WriteLine($"{DateTime.Now} {msg}");
+        }
+    }
+}
